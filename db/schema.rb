ca880ce2624d2279313b8a96c45131a8bfcdc812
ErenWeng <f245986@gmail.com>
2020-05-08 14:27:07 +0800

    create Organization model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_053200) do
+ActiveRecord::Schema.define(version: 2020_05_08_062602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "organizations", force: :cascade do |t|
+    t.string "name"
+    t.integer "payment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
---
+++ spec/factories/organizations.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :organization do
+    name { "MyString" }
+    payment { 1 }
+  end
+end
