23455b491f33539777c96615abd75c541f6f2318
koten0224 <koten0224@gmail.com>
2020-05-10 04:38:50 +0800

    免除N+1 query從手刻sql做起！把user底下的organizations方法做改寫使其可以在查詢時一併帶入權限查詢，另一邊的organization底下的users方法也一樣

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,4 @@
+#每次豪華福州校園隨意紛紛財政群眾最後裝飾回到這是地說那天坐。
 class Organization < ApplicationRecord
   #associations
   has_many :organizations_users, dependent: :destroy
@@ -5,4 +6,31 @@ class Organization < ApplicationRecord
   
   #validations
   validates :name, presence: true, uniqueness: true, length: { minimum: 1, maximum: 100 }
+
+  attr_accessor :purview #權限
+  
+  alias_method :native_users, :users
+  def users
+    result = User.find_by_sql("
+        select users.*,rel.role
+        from users
+        inner join organizations_users rel
+        on users.id=rel.user_id 
+        inner join organizations org
+        on org.id=rel.organization_id 
+        where org.id=#{id}
+      ")
+    result.each do |user|
+      user.role = user.attributes["role"]
+    end
+    return result
+  end
 end
+
+# select users.id,users.email,users.name,rel.role
+# from users
+# inner join organizations_users rel
+# on users.id=rel.user_id 
+# inner join organizations org
+# on org.id=rel.organization_id 
+# where org.id=5
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,23 @@ class User < ApplicationRecord
   #validations
   validates :email, presence: true, uniqueness: true, length: { minimum: 1, maximum: 100 }
   validates :encrypted_password, presence: true, length: { minimum: 6, maximum: 128 }
+
+  attr_accessor :role
+  
+  alias_method :native_organizations, :organizations
+  def organizations
+    result = Organization.find_by_sql("
+        select org.*,rel.role
+        from organizations org
+        inner join organizations_users rel
+        on org.id=rel.organization_id 
+        inner join users
+        on users.id=rel.user_id 
+        where users.id=#{id}
+      ")
+      result.each do |organization|
+        organization.purview = organization.attributes["role"]
+      end
+      return result
+  end
 end
