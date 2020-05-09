bcc8abbd5000618009b28ce0015ecf89240cd5ec
RocketSH <57695762+RocketSH@users.noreply.github.com>
2020-05-09 11:40:55 +0800

    Improve email and password validation
    1.Password validation needs to be done through Devise configuration.
    Test in: https://regexr.com/
    2. It is not necessary to set a minimal length of 1 with `validates :presence`
    See: [devise docs](https://github.com/heartcombo/devise/wiki/Customize-minimum-password-length)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, password_length: 8..100
   
   #associations
   has_many :user_organization_refs
   has_many :organizations, through: :user_organization_refs
 
   #validations
-  validates :email, presence: true, uniqueness: true, length: { minimum: 1, maximum: 100 }
-  validates :encrypted_password, presence: true, length: { minimum: 1, maximum: 100 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 100 },
+            format: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  # validates :encrypted_password, presence: true, length: { minimum: 1, maximum: 100 }
 end
