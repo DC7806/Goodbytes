55bb5f01b807a42e03f3155f1ffbf0f36bd219cf
RocketSH <57695762+RocketSH@users.noreply.github.com>
2020-05-09 11:51:42 +0800

    Set DB constraints in organizations table
    1.  name should be non-nullable and unique(add_index)
    2. payment or paid? should be not-nullable as well XD

--- db/migrate/20200508062602_create_organizations.rb
+++ db/migrate/20200508062602_create_organizations.rb
@@ -1,10 +1,12 @@
 class CreateOrganizations < ActiveRecord::Migration[6.0]
   def change
     create_table :organizations do |t|
-      t.string :name
-      t.integer :payment
+      t.string :name, null: false
+      t.integer :payment, null: false, default: 0
 
       t.timestamps
     end
+    
+    add_index :organizations, :name, unique: true
   end
 end
