6ba0b76cd0faa6bd294cc688eb833f177603a197
koten0224 <koten0224@gmail.com>
2020-05-09 23:09:40 +0800

    建立devise:user的controller並在創帳號時送組織，在user新增名稱欄位，在中間表格新增role欄位

--- app/models/organizations_user.rb
+++ app/models/organizations_user.rb
@@ -1,6 +1,6 @@
 class OrganizationsUser < ApplicationRecord
-  belongs_to :organization
-  belongs_to :user
+  belongs_to :organization, dependent: :destroy
+  belongs_to :user, dependent: :destroy
 
   validates :user, presence: true
   validates :organization, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_120923) do
+ActiveRecord::Schema.define(version: 2020_05_09_141842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_05_08_120923) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "role"
     t.index ["organization_id"], name: "index_organizations_users_on_organization_id"
     t.index ["user_id"], name: "index_organizations_users_on_user_id"
   end
@@ -42,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_05_08_120923) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
