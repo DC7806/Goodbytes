ad3113737fe2bcf5b3c5bfe421ec922ba6ff44eb
koten0224 <koten0224@gmail.com>
2020-05-13 22:54:56 +0800

    refactor tmp

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -6,4 +6,18 @@ class Channel < ApplicationRecord
   has_many :users, through: :organizations_users
 
   belongs_to :organization
+
+  def role(user_id)
+    result = ChannelsOrgUser.find_by_sql("
+      select * 
+      from channels_org_users ch
+      inner join organizations_users org
+      on ch.organizations_user_id = org.id
+      inner join users
+      on users.id = #{user_id} and users.id = org.user_id
+      where ch.id = #{id}
+      ").first
+    return result && result.role || "None"
+  end
+
 end
