c80ef0302ed7707caa789974170fed915d9109a5
koten0224 <koten0224@gmail.com>
2020-05-14 11:59:02 +0800

    Channels controller crud功能完成 目前功能暫時放在feature2

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,6 +1,5 @@
 class ChannelsController < ApplicationController
   before_action :find_channel, except: [:create]
-  after_action :back_to_root, except: [:find_channel, :channel_params]
 
   def show
   end
@@ -11,6 +10,7 @@ class ChannelsController < ApplicationController
     else
       @notice = "channel新增失敗"
     end
+    redirect_to(root_path, notice: @notice) and return
   end
   def update
     if @channel.update(channel_params)
@@ -18,6 +18,7 @@ class ChannelsController < ApplicationController
     else
       @notice = "channel更新失敗"
     end
+    redirect_to(root_path, notice: @notice) and return
   end
   def destroy
     if @channel.destroy
@@ -25,6 +26,7 @@ class ChannelsController < ApplicationController
     else
       @notice = "channel刪除失敗"
     end
+    redirect_to(root_path, notice: @notice) and return
   end
 
   private
@@ -34,11 +36,9 @@ class ChannelsController < ApplicationController
   def channel_params
     params.require(:channel).permit(
       :name,
-      :description
+      :description,
+      :organization_id
     )
-  end
-  def back_to_root
-    redirect_to root_path, notice: @notice
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,26 @@
 Rails.application.routes.draw do
-  get 'channels/create'
-  get 'channels/update'
-  get 'channels/destroy'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   
-  root   "dashboard#index"
-  get    "/feature1",               to: "test#feature1"
-  post   "/invite/send",            to: "invites#create"
-  delete "/invite/cancel",          to: "invites#destroy"
-  get    "/organization/join",      to: "invites#join_to_organization"
-  get    "/organization/invited",   to: "invites#sign_up_and_join"
-  post   "/organization/promotion", to: "organization_roles#update"
-  delete "/organization/fire",      to: "organization_roles#destroy"
-  get    "/channel/:channel_id",    to: "channels#show"
+  root    "dashboard#index"
+
+  get     "/feature1",               to: "test#feature1"
+  get     "/feature2",               to: "test#feature2"
+
+  post    "/invite/send",            to: "invites#create"
+  delete  "/invite/cancel",          to: "invites#destroy"
+
+  get     "/organization/join",      to: "invites#join_to_organization"
+  get     "/organization/invited",   to: "invites#sign_up_and_join"
+  post    "/organization/promotion", to: "organization_roles#update"
+  delete  "/organization/fire",      to: "organization_roles#destroy"
+
+  get     "/channel/:channel_id",    to: "channels#show",               as: "channel"
+  post    "/channel",                to: "channels#create",             as: "channel_create"
+  post    "/channel/update",         to: "channels#update",             as: "channel_update"
+  delete  "/channel",                to: "channels#destroy",            as: "channel_destroy"
+
+  # resources :channels, path: "/channel", only: [:show, :create, :update, :destroy]
+  
   # devise_scope :users do
   #   post "/users/sign_up" => "users/registrations#invited"
   # end
