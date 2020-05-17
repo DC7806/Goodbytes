1f53d6388a6569c5fc8ef939ff9e6a62b14cded3
Shuhan Hu <1119han@gmail.com>
2020-05-17 18:24:32 +0800

    Add constraints to articles

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,12 @@ ActiveRecord::Schema.define(version: 2020_05_13_140251) do
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
-    t.integer "channel_id"
-    t.string "subject"
+    t.integer "channel_id", null: false
+    t.string "subject", null: false
     t.datetime "deliver_time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["channel_id"], name: "index_articles_on_channel_id"
   end
 
   create_table "channels", force: :cascade do |t|
@@ -88,6 +89,7 @@ ActiveRecord::Schema.define(version: 2020_05_13_140251) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "channels"
   add_foreign_key "channels", "organizations"
   add_foreign_key "channels_org_users", "channels"
   add_foreign_key "channels_org_users", "organizations_users"
