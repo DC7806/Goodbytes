5930a760356171eadd530f3709fb4aad6ac4103c
ErenWeng <f245986@gmail.com>
2020-05-18 14:48:10 +0800

    fix model association and link_groups_controller index for @channel

--- app/controllers/link_groups_controller.rb
+++ app/controllers/link_groups_controller.rb
@@ -1,6 +1,9 @@
 class LinkGroupsController < ApplicationController
+  before_action :find_channel
+  before_action :find_link_group, only: [:edit, :update, :destroy]
+  
   def index
-    @link_groups = LinkGroup.order(created_at: :asc).includes(:saved_links)
+    @link_groups = @channel.link_groups.order(created_at: :asc).includes(:saved_links)
     @saved_link = SavedLink.new
   end
 
@@ -20,11 +23,9 @@ class LinkGroupsController < ApplicationController
   end
 
   def edit
-    @link_group = LinkGroup.find(params[:id])
   end
 
   def update
-    @link_group = LinkGroup.find(params[:id])
 
     if @link_group.update(link_group_params)
       redirect_to channel_link_groups_path
@@ -34,7 +35,6 @@ class LinkGroupsController < ApplicationController
   end
 
   def destroy
-    @link_group = LinkGroup.find(params[:id])
     if @link_group.destroy 
       redirect_to channel_link_groups_path
     end
@@ -43,5 +43,13 @@ class LinkGroupsController < ApplicationController
   private
   def link_group_params
     params.require(:link_group).permit(:name)
+  end
+
+  def find_channel
+    @channel = Channel.find(params[:channel_id])
+  end
+
+  def find_link_group
+    @link_group = LinkGroup.find(params[:id])
   end
 end
--- app/models/channel.rb
+++ app/models/channel.rb
@@ -5,9 +5,10 @@ class Channel < ApplicationRecord
   has_many :organizations_users, through: :channels_org_users
   has_many :users, through: :organizations_users
 
-  has_many :link_groups
+  has_many :link_groups, dependent: :destroy
 
   has_many :articles
+  has_many :saved_links, through: :link_groups
 
 
   belongs_to :organization
