af7071b81e9b5621cb5d3c6d95163322d30600f1
ErenWeng <f245986@gmail.com>
2020-05-23 19:39:13 +0800

    link AJAX OK but routes still not fix

--- app/controllers/link_groups_controller.rb
+++ app/controllers/link_groups_controller.rb
@@ -2,6 +2,7 @@ class LinkGroupsController < ApplicationController
   before_action :find_link_group, only: [:edit, :update, :destroy]
   before_action :find_channel
   before_action :channel_member?
+  before_action :for_ajax_render_find, only: [:create, :update, :destroy]
   
   def index
     @link_groups = @channel.link_groups.order(created_at: :asc).includes(:saved_links)
@@ -17,10 +18,6 @@ class LinkGroupsController < ApplicationController
     @link_group            = LinkGroup.new(link_group_params)
     @link_group.channel_id = params[:channel_id]
 
-    # for AJAX render
-    @link_groups = @channel.link_groups.order(created_at: :asc).includes(:saved_links)
-    @saved_link = SavedLink.new
-
     if @link_group.save
       @ajax_create_group = { ok: true }
     else
@@ -32,10 +29,6 @@ class LinkGroupsController < ApplicationController
   end
 
   def update
-    # for AJAX render
-    @link_groups = @channel.link_groups.order(created_at: :asc).includes(:saved_links)
-    @saved_link = SavedLink.new
-
     if @link_group.update(link_group_params)
       @link_group = LinkGroup.new
       @ajax_update_group = { ok: true }
@@ -45,14 +38,11 @@ class LinkGroupsController < ApplicationController
   end
 
   def destroy
-    # for AJAX render
-    @link_groups = @channel.link_groups.order(created_at: :asc).includes(:saved_links)
-    @saved_link = SavedLink.new
-
     if @link_group.destroy 
       @ajax_destroy_group = { ok: true }
     end
   end
+
 
   private
   def link_group_params
@@ -62,5 +52,10 @@ class LinkGroupsController < ApplicationController
   def find_link_group
     @link_group = LinkGroup.find(params[:id])
     @subobject = @link_group
+  end
+
+  def for_ajax_render_find
+    @link_groups = @channel.link_groups.order(created_at: :asc).includes(:saved_links)
+    @saved_link = SavedLink.new
   end
 end
