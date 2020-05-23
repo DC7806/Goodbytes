8fef460f5883dabcc3a6699f0bdf7e04f485ced5
Shuhan Hu <1119han@gmail.com>
2020-05-23 14:37:03 +0800

    Add default content column to Article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_085639) do
+ActiveRecord::Schema.define(version: 2020_05_23_063419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 2020_05_22_085639) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "deleted_at"
+    t.text "header"
+    t.text "footer"
+    t.text "main_img"
     t.index ["channel_id"], name: "index_articles_on_channel_id"
     t.index ["deleted_at"], name: "index_articles_on_deleted_at"
   end
