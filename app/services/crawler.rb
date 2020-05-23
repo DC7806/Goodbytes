70984fcb1ac7e3d2487c3135f00663fb3a06aa72
koten0224 <koten0224@gmail.com>
2020-05-23 13:04:23 +0800

    依dc建議修改： 去掉get方法，整理json格式就請各自在controller裡做囉 subject跟summary加上edge case處理 要是找不到的話預計是會在form出現要求使用者增加內容的字樣 summary長度限制改為200 去掉require nokogiri

--- app/services/crawler.rb
+++ app/services/crawler.rb
@@ -1,29 +1,31 @@
 class Crawler
+  require "open-uri"
   
   def initialize(url)
-    require 'nokogiri'
-    require 'open-uri'
     @page = Nokogiri::HTML(open(url, headers).read)
   end
 
   def subject
-    @page.css("title").first.text
+    result = @page.css("title").first
+    if result && result.text.present?
+      result.text
+    else
+      "This page has no title. Maybe you should append some of it."
+    end
   end
 
   def summary
-    @page.css("meta[name=description]").first.attr("content")[0..1000]
-  end
-
-  def get
-    {
-      subject: subject,
-      summary: summary
-    }
+    result = @page.css("meta[name=description]").first
+    if result && result.attr("content").present?
+      result.attr("content")[0..200]
+    else
+      "This page has no summary. Maybe you should append some of it."
+    end
   end
 
   private
   def headers
-    return {'User-Agent' => 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Mobile Safari/537.36'}
+    {'User-Agent' => 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Mobile Safari/537.36'}
   end
 
 end
@@ -35,8 +37,3 @@ end
 # crawler = Crawler.new(url)
 # p crawler.subject# => "五倍紅寶石｜專業程式教育機構｜實戰轉職訓練"
 # p crawler.summary# => "五倍紅寶石擁有業界強大師資群，程式課程多元豐富，提供實體線上課程選擇，由20年業界經驗的高見龍領軍，經驗值滿點助教群，專業年輕的服務團隊。"
-# p crawler.get# =>
-# # {
-# #   subject: "五倍紅寶石｜專業程式教育機構｜實戰轉職訓練", 
-# #   summary: "五倍紅寶石擁有業界強大師資群，程式課程多元豐富，提供實體線上課程選擇，由20年業界經驗的高見龍領軍，經驗值滿點助教群，專業年輕的服務團隊。"
-# # }
