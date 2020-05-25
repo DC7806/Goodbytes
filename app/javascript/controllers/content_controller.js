21ad1770350b508bb51fd77fffe96596d4e8e46d
koten0224 <koten0224@gmail.com>
2020-05-25 13:46:20 +0800

    修改js controller

--- app/javascript/controllers/content_controller.js
+++ app/javascript/controllers/content_controller.js
@@ -1,18 +1,30 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["aa", "bb"]  
+  static targets = ["editor"] 
 
-  blabla(e){   
-    console.log(this) 
-    console.log(e)
-    this.myCartTarget.classList.add('shake');
-    this.cartCounterTarget.innerText = e.detail; 
-    let self = this;
+  connect(){
+    console.log("hi")
+  //   window.sortable = Sortable.create(editor,{
+  //     group: "shared",
+  //         animation: 250,
+  //         ghostClass: "blue-background-class",
+  //         onUpdate: function (e) {
+  //           console.log(e.item)
+  //         },
+  //         onAdd: function (e) {
+  //           console.log(e.target.querySelector("div.sortable-choosen"));
+  //         },
+  //   })
+  //   console.log(this) 
+  //   console.log(e)
+  //   this.myCartTarget.classList.add('shake');
+  //   this.cartCounterTarget.innerText = e.detail; 
+  //   let self = this;
 
-    setTimeout(function(){
-      self.myCartTarget.classList.remove('shake');
-    }, 500);
+  //   setTimeout(function(){
+  //     self.myCartTarget.classList.remove('shake');
+  //   }, 500);
 
   }
 
