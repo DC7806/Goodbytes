9d17ec12c50546601dbbaf1de930faf045e4ce3f
koten0224 <koten0224@gmail.com>
2020-05-25 18:00:20 +0800

    安裝fontawsome 增加content controller

--- app/javascript/controllers/drag_controller.js
+++ app/javascript/controllers/drag_controller.js
@@ -52,21 +52,8 @@ export default class extends Controller {
           })
         }
         updatePosition(0)
-        // contentsLoad()
       },
-      // onAdd: function (e) {
-      //   console.log(e.target.querySelector("div.sortable-choosen"));
-      // },
     })
-  //   console.log(this) 
-  //   console.log(e)
-  //   this.myCartTarget.classList.add('shake');
-  //   this.cartCounterTarget.innerText = e.detail; 
-  //   let self = this;
-
-  //   setTimeout(function(){
-  //     self.myCartTarget.classList.remove('shake');
-  //   }, 500);
 
   }
 }
