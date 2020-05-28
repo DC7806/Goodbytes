637e9ec9da98fb81e6e0fb184f55ff341ade07d6
koten0224 <koten0224@gmail.com>
2020-05-28 15:12:10 +0800

    content add column image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_100446) do
+ActiveRecord::Schema.define(version: 2020_05_28_071125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_05_27_100446) do
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "deleted_at"
     t.integer "layout"
+    t.string "image"
     t.index ["deleted_at"], name: "index_contents_on_deleted_at"
   end
 
