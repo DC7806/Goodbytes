675875ff0b781c5022ffdd7dc0b724afefdb31c1
koten0224 <koten0224@gmail.com>
2020-05-28 18:21:45 +0800

    設定圖片壓縮尺寸 並且在edit form與文章顯示兩種尺寸

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,12 +1,17 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
 
+  process resize_to_limit: [600, 600]
+
+  version :form do
+    process resize_to_limit: [200,200]
+  end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
