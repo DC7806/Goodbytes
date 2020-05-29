d4c8a25d6803f3a5e3931bb16be5c06c879b7f3a
koten0224 <koten0224@gmail.com>
2020-05-29 21:56:05 +0800

    改寫sinebar switch 一半

--- app/javascript/controllers/switch_controller.js
+++ app/javascript/controllers/switch_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "stimulus"
 import Rails from "@rails/ujs"
 
 export default class extends Controller {
-  static targets = ["form"] 
+  static targets = ["form", "objectId"] 
   
   edit(evt){
     evt.preventDefault()
@@ -19,6 +19,33 @@ export default class extends Controller {
     $("#back").hide()
     $(".edit_content").hide()
     $("#drag-area").show()
+  }
+  
+  toggleOrgList(evt){
+    evt.preventDefault()
+    $("#" + this.objectIdTarget.value).toggle()
+  }
+  
+  organization(evt){
+    evt.preventDefault()
+    let organizationId = evt.target.dataset.id
+    let data = JSON.stringify({id: organizationId})
+    Rails.ajax({
+      url: `/switch_organization`,
+      type: 'POST', 
+      dataType: 'json',
+      beforeSend: (xhr, options) => {
+        options.data = data
+        xhr.setRequestHeader('Content-Type', 'application/json')
+        return true
+      },
+      success: resp => {
+        window.location.replace("/channel");
+      }, 
+      error: err => {
+        console.log(err);
+      } 
+    })
   }
 
 
