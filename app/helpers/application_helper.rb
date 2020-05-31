d2c015a4acac2cd62da0d0606fd2eb998046d558
ErenWeng <f245986@gmail.com>
2020-05-31 19:41:56 +0800

    fix top_bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,25 @@
 module ApplicationHelper
 
+  # 根據當前時間來打招呼
+  def greet
+    now = Time.now
+    today = Date.today.to_time
+  
+    morning = today.beginning_of_day
+    noon = today.noon
+    evening = today.change( hour: 17 )
+    night = today.change( hour: 20 )
+    tomorrow = today.tomorrow
+  
+    if (morning..noon).cover? now
+      'Good morning'
+    elsif (noon..evening).cover? now
+      'Good afternoon'
+    elsif (evening..night).cover? now
+      'Good evening'
+    elsif (night..tomorrow).cover? now
+      'Good night'
+    end
+  end
+  
 end
