e7055ccf5659cb60381f92a817e100b8b4695038
koten0224 <koten0224@gmail.com>
2020-06-05 12:31:56 +0800

    mailer set default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'https://www.goodbyt.es/' }
   config.action_mailer.smtp_settings = {
     address:              'smtp.mailgun.org',
     port:                 587,
